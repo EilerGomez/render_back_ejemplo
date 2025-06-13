0a0961d1780edef7b1a2084e4757e3a89ca047c3
EilerGomez <gomezeiler250@gmail.com>
2025-06-12 18:52:05 -0600

    funcion get por medio de un id de personas

--- app.js
+++ app.js
@@ -41,6 +41,21 @@ app.get('/personas', async (req, res) => {
     res.status(500).json({ error: err.toString() })
   }
 })
+// Traer una persona por su id
+app.get('/personas/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query('SELECT * FROM Persona WHERE id = $1', [id]);
+    
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Persona no encontrada' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message }); // Usar err.message en lugar de err.toString()
+  }
+});
 
 // Actualizar Persona
 app.put('/personas/:id', async (req, res) => {
